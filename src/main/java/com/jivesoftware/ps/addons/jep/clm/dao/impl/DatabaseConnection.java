1568fdc1091a8f613b6873ac1628fa7e84d2f9a6
Rup Utpal Paul <ruputpalpaul29@gmail.com>
2022-07-07 00:59:19 +0530

    created classes

--- src/main/java/com/jivesoftware/ps/addons/jep/clm/dao/impl/DatabaseConnection.java
+++ src/main/java/com/jivesoftware/ps/addons/jep/clm/dao/impl/DatabaseConnection.java
@@ -1,21 +1,27 @@
 package com.jivesoftware.ps.addons.jep.clm.dao.impl;
 
-import java.sql.Array;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
+
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
+
 
 public class DatabaseConnection {
 	
 	private final DBI dbi = new DBI("link of db goes here");
+	List<String> result = null;
 	
-	Handle getDatabase() {
+	List<String> getDatabase() {
+		
 		Handle h = dbi.open();
 		
-		return h;
+		List<String> result = h.createQuery("select name from something order by id")
+					.map(String.class)
+					.list();
+        h.close();
+
+        return result;
 	}
 	
 }
