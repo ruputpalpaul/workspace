f5674d045fbe1826be215e3f30849337215ac737
Rup Utpal Paul <ruputpalpaul29@gmail.com>
2022-07-07 02:14:22 +0530

    local

--- src/main/java/com/jivesoftware/ps/addons/jep/clm/dao/impl/DatabaseConnection.java
+++ src/main/java/com/jivesoftware/ps/addons/jep/clm/dao/impl/DatabaseConnection.java
@@ -9,16 +9,17 @@ import org.skife.jdbi.v2.Handle;
 
 public class DatabaseConnection {
 	
-	private final DBI dbi = new DBI("postgres://YourUserName:YourPassword@localHost:5432/YourDatabaseName");
+	private final DBI dbi = new DBI("postgres://postgres:Hiy@1329@localHost:5432/mydb");
 	List<String> result = null;
 	
 	List<String> getDatabase() {
 		
 		Handle h = dbi.open();
 		
-		List<String> result = h.createQuery("select name from something order by id")
+		List<String> result = h.createQuery("select * from test;")
 					.map(String.class)
 					.list();
+		System.out.print(result);
         h.close();
 
         return result;
