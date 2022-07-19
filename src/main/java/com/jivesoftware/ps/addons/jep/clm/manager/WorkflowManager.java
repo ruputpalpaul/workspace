de63a789b84035668fc018ef786d3637fc9f3f03
ruputpalpaul <ruputpalpaul29@gmail.com>
2022-07-19 12:10:13 +0530

    added manager class

---
+++ src/main/java/com/jivesoftware/ps/addons/jep/clm/manager/WorkflowManager.java
@@ -0,0 +1,6 @@
+package com.jivesoftware.ps.addons.jep.clm.manager;
+
+public class WorkflowManager {
+    
+    public AixListModel<WorkflowModel> listWorkflows (WorkflowType type, Integer count, Integer startIndex, JiveObject jiveInstance)
+}
